ea2eeb9e6377ad8f0e38f030acd97ee62143d14e
Bossbadr <100685115+Bossbadr@users.noreply.github.com>
2022-03-28 15:15:46 +0000

    Update Java.java

--- Java.java
+++ Java.java
@@ -2,7 +2,7 @@ public class Java {
 
 	public static void main(String[] args) {
 		String xxx = "Badr el Hamdouni";
-		System.out.println("Hola Jenkins, soc " +  xx);
+		System.out.println("Hola Jenkins, soc " +  xxx);
 
 	}
 
